75af33b41dd5820c79f866d7248f1ded6aa8303f
tephimano <tephimano@gmail.com>
2020-08-18 15:05:49 -0400

    new-branch.js added

---
+++ src/app/MessageTable.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+const DisplayMessageTable = () => {
+  return <div>Display a message Table</div>;
+};
+
+export default DisplayMessageTable;
---
+++ src/newBanch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const newBranch = () => {
+  return <div>Hello</div>;
+};
+
+export default newBranch;
